0055bad3ed2dec213f98ab73ecbe9331b365dc48
Untetzy <notescape262@gmail.com>
2021-04-04 17:13:44 +0200

    onclick

--- src/components/main/page/profile/profile-right/profile-posts/posts-form/posts-form.jsx
+++ src/components/main/page/profile/profile-right/profile-posts/posts-form/posts-form.jsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import './posts-form.css';
 
 const PostsForm = () => {
+
+  let addTextEl = React.createRef();
+
+  let addPost = () => {
+    let text = addTextEl.current.value;
+    alert(text);
+  }
+
   return (
     <form className="posts__form" action="#">
-      <textarea className="posts__form-textarea" placeholder="Anything new?"></textarea>
-      <button className="posts__form-btn" type="submit">Submit</button>
+      <textarea className="posts__form-textarea" placeholder="Anything new?" ref={addTextEl}></textarea>
+      <button className="posts__form-btn" type="submit" onClick={addPost}>Submit</button>
     </form>
   );
 }
